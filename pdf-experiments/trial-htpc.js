58ea57fb9b0f1b54e6edcea02f26afee4280e1be
asmitachavan121 <asmitachavan121@gmail.com>
2020-11-17 00:00:13 +0530

    add trial file for html-to-pdf-converter npm pkg

---
+++ pdf-experiments/trial-htpc.js
@@ -0,0 +1,104 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+/**
+ * observations 
+ * pdf output is buggy - tried buffer to string etc, but pdf content invalid error occured
+ */
+const fs = require('fs')
+const handlebars = require('handlebars')
+const pdf = require('html-to-pdf-converter')
+const htmlTemplate = fs.readFileSync('trial.html', 'utf-8')
+  handlebars.registerHelper('print_person', function () {
+    return this.firstname + ' ' + this.lastname
+})
+
+const firstPage = handlebars.compile(htmlTemplate)({
+    people: [
+        {
+          firstname: "Nils",
+          lastname: "Knappmeier",
+        },
+        {
+          firstname: "Yehuda",
+          lastname: "Katz",
+        },
+      ],
+})
+
+function splitOrders(orders, columnsCount) {
+  const keys = Object.keys(orders[0])
+  const firstPageFields = keys.slice(0,columnsCount)
+  const secondPageFields = keys.slice(columnsCount,)
+  let firstPageOrders = []
+  let secondPageOrders = []
+  orders.forEach(order => {
+    let partialOrder = {}
+    firstPageFields.forEach(field => {
+      partialOrder[field] = order[field]
+    })
+    firstPageOrders.push(partialOrder)
+
+  })
+  orders.forEach(order => {
+    let partialOrder = {}
+    secondPageFields.forEach(field => {
+      partialOrder[field] = order[field]
+    })
+    secondPageOrders.push(partialOrder)
+  })
+  return { firstPageOrders, secondPageOrders}
+}
+
+// const orders = JSON.parse(fs.readFileSync('orders.json', 'utf-8'))
+// const { firstPageOrders, secondPageOrders } = splitOrders(orders, 7)
+// console.log('first page orders = ', firstPageOrders)
+// console.log('second page orders = ', secondPageOrders)
+// fs.writeFileSync('htmlFile.html', firstPage, 'utf-8')
+
+async function createPdf(firstPage) {
+  const margin = pdf.PrintMargin.ofMillimeters({
+    top: 7.5,
+    right: 15,
+    bottom: 5,
+    left: 17,
+  })
+  const puppeteerOptions = {
+    puppeteer: {
+      args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage'],
+    },
+  }
+  const converter = await pdf.initialize(puppeteerOptions)
+  const pdfBuffer = await converter.mkCompoundPdf([
+    {
+      pdfContent: firstPage,
+      margin,
+      pageSize: pdf.pageSizeInMM.A4.portrait,
+    },
+    {
+      pdfContent: fs.readFileSync('restpage.html').toString(),
+      margin,
+      pageSize: pdf.pageSizeInMM.A4.portrait,
+    }
+  ])
+  console.log(pdfBuffer)
+
+  converter.destroy()
+  return pdfBuffer
+}
+try {
+  const pdfContent = createPdf(firstPage)
+  const content = pdfContent.toString()
+  fs.writeFileSync('result.pdf', content, 'utf-8')
+}catch(error) {
+  throw(error)
+}
+// var options = {
+//   format: 'A4',
+//   contents: fs.readFileSync('trial.html', 'utf-8'),
+//   // phantomPath: './node_modules/phantomjs-prebuilt/bin/phantomjs'
+// }
+// pdf.create(html, options).toFile('./businesscard.pdf', function(err, res) {
+//   if (err) return console.log(err);
+//   console.log(res); // { filename: '/app/businesscard.pdf' }
+// });
+
